1c9c555e62d620b01806b2ed1421361590d107be
Olivia Feldman <orwfeldman@gmail.com>
2024-01-15 17:09:25 +0100

    setup App component with weather API, added state and prevent default

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [query, setQuery] = ('');
+  const [weather, setWeather] = [];
+
+  const searchWeather = async (e) => {
+    e.preventDefault();
+
+  const url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m';
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    setWeather(data.results);
+  } catch (err) {
+    console.log(err);
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <h1>Weather App</h1>
+      </div><div>
+          <form>
+            <label className='label' htmlFor='query'>City</label>
+            <input className='input' type='text' name='query'
+              placeholder='i.e. Berlin'>
+            </input>
+            <button className="button" type="submit">Search</button>
+          </form>
+        </div>
+      </>
+  )
 }
 
-export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
